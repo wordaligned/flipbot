56fd2a6175a0e2fc307fcfd03dd630f86a90e09f
Thomas Guest <tag@wordaligned.org>
2017-05-01 20:13:53 +0100

    Refactored client calls into a class. Gather user data.

--- flipbot.py
+++ flipbot.py
@@ -1,4 +1,7 @@
-''' Flipbot flips text and images posted to slack channels. '''
+''' Flipbot flips text and images posted to slack channels. 
+
+Thomas Guest, https://github.com/wordaligned/flipbot
+'''
 
 import configparser
 import io
@@ -22,6 +25,88 @@ config.read('settings.ini')
 TOKEN = config['SETTINGS']['TOKEN']
 USER = config['SETTINGS']['USER']
 
+class FlipClient:
+    '''Slack RTM client which flips messages.'''
+
+    def __init__(self, token, user):
+        self._client = slackclient.SlackClient(token)
+        self._user = user
+        self._api_call = self._client.api_call
+        if self._client.rtm_connect():
+            print("Flipbot connected and running!")
+            self._find_users()
+        else:
+            print("Connection failed. Invalid Slack token or bot ID?")
+
+    def _react(self, msg):
+        '''React to the message with a flipped emoji.'''
+        self._api_call('reactions.add',
+                       channel=msg['channel'],
+                       timestamp=msg['ts'],
+                       name=reaction())
+
+    def _flip_image_message(self, msg):
+        '''Respond to an image upload by posting a flipped version.'''
+        f = msg['file']
+        fname = f['name']
+        url = f['url_private_download']
+        meta = flip_file_metadata(f)
+
+        hdrs = {'Authorization': 'Bearer %s' % TOKEN}
+        resp = requests.get(url, headers=hdrs)
+        if resp.status_code == 200:
+            self._api_call('files.upload',
+                           filename=flip_text(fname),
+                           channels=msg['channel'],
+                           file=flip_image(resp.content),
+                           **meta)
+        else:
+            print('Download failed', resp.content)
+
+    def _flip_text_message(self, msg):
+        '''Respond to the text message by posting a flipped version.'''
+        text = msg['text']
+        self._api_call('chat.postMessage',
+                       channel=msg['channel'],
+                       text=flip_text_with_links(text),
+                       as_user=True)
+
+    def _find_users(self):
+        r = self._client.api_call('users.list')
+        if r['ok']:
+            self._users = {m['id']: m['name'] for m in r['members']}
+
+    def _messages(self):
+        while True:
+            yield from self._client.rtm_read()
+            time.sleep(1)
+
+    def _handle(self, msg):
+        try:
+            if msg.get('user') == self._user:
+                pass # Don't reprocess our own messages!
+            elif is_image_message(msg):
+                self._flip_image_message(msg)
+                self._react(msg)
+            elif is_text_message(msg):
+                self._flip_text_message(msg)
+                self._react(msg)
+        except Exception as e:
+            print(e, file=sys.stderr)
+
+    def run(self):
+        for msg in self._messages():
+            if is_user_change(msg):
+                self._find_users()
+            self._handle(msg)
+
+def is_user_change(msg):
+    '''Return true if the users have been changed.'''
+    return msg['type'] in {'user_change',
+                           'team_join',
+                           'bot_added',
+                           'bot_updated'}
+
 def reaction():
     '''Return a reaction (emoji)'''
     return random.choice(
@@ -39,9 +124,9 @@ link_re = re.compile('<(https?://[^|>]*)(\|?)([^>]*)>')
 
 def flip_text_with_links(s, flip_fn=flip_text, echo_fn=echo_text):
     '''Handles links in the message, s.
-    
+
     See: https://api.slack.com/docs/message-formatting#linking_to_urls
-       
+
 
     Any URL in s will look something like <http://www.foo.com|www.foo.com>
     We keep the link functional by flipping the text following the pipe,
@@ -71,14 +156,6 @@ def is_image_message(msg):
             msg.get('subtype') == 'file_share' and
             msg['file']['mimetype'].startswith('image'))
 
-def flip_text_message(client, msg):
-    '''Respond to the text message by posting a flipped version.'''
-    text = msg['text']
-    client.api_call('chat.postMessage',
-                    channel=msg['channel'],
-                    text=flip_text_with_links(text),
-                    as_user=True)
-
 def flip_file_metadata(f):
     '''Returns flipped upload file metadata.'''
     meta = {}
@@ -87,7 +164,7 @@ def flip_file_metadata(f):
     if title:
         meta['title'] = flip_text(title)
     if comment:
-        meta['initial_comment'] = flip_links(comment)
+        meta['initial_comment'] = flip_text_with_links(comment)
     return meta
 
 def flip_image(img_bytes):
@@ -100,57 +177,6 @@ def flip_image(img_bytes):
     stream.seek(0)
     return stream.read()
 
-def flip_image_message(client, msg):
-    '''Respond to an image upload by posting a flipped version.'''
-    f = msg['file']
-    fname = f['name']
-    url = f['url_private_download']
-    meta = flip_file_metadata(f)
-
-    hdrs = {'Authorization': 'Bearer %s' % TOKEN}
-    resp = requests.get(url, headers=hdrs)
-    if resp.status_code == 200:
-        client.api_call('files.upload',
-                        filename=flip_text(fname),
-                        channels=msg['channel'],
-                        file=flip_image(resp.content),
-                        **meta)
-    else:
-        print('Download failed', resp.content)
-
-def react(client, msg):
-    '''React to the message with a flipped emoji.'''
-    client.api_call('reactions.add',
-                    channel=msg['channel'],
-                    timestamp=msg['ts'],
-                    name=reaction())
-
-def handle(client, messages):
-    '''Respond to a list of messages.'''
-    for msg in messages:
-        try:
-            if msg.get('user') == USER:
-                pass # Don't reprocess our own messages!
-            else:
-                if is_image_message(msg):
-                    flip_image_message(client, msg)
-                    react(client, msg)
-                elif is_text_message(msg):
-                    flip_text_message(client, msg)
-                    react(client, msg)
-        except Exception as e:
-            print(e, file=sys.stderr)
-
-def run(client):
-    '''Run flipbot.'''
-    while True:
-        handle(client, client.rtm_read())
-        time.sleep(1)
-
 if __name__ == "__main__":
-    client = slackclient.SlackClient(TOKEN)
-    if client.rtm_connect():
-        print("Flipbot connected and running!")
-        run(client)
-    else:
-        print("Connection failed. Invalid Slack token or bot ID?")
+    client = FlipClient(TOKEN, USER)
+    client.run()
